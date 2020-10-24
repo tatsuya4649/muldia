89fdb13651b0faf4c0ed82722bc3392d5fff398c
tatsuya <tatsuya4946developer@gmail.com>
2020-10-25 01:01:00 +0900

    shape maked

--- include/core/shape.h
+++ include/core/shape.h
@@ -13,6 +13,8 @@
 namespace __md{
 	using __shape_t = unsigned int;
 	using __shape_size_t = unsigned int;
+	using __shape = std::vector<__shape_t>;
+	using __shape_list = std::initializer_list<__shape_t>;
 	class shape{
 		private:
 			bool chck_shape_size(__shape_t i){
@@ -26,11 +28,11 @@ namespace __md{
 			// 
 			// how to initialize muldia::shape<T> class
 			// =========================================
-			// muldia::shape<T>([a,b,c]) => shape (a,b,c)
-			// muldia::shape<T>(std::vector<T>(a,b,c)) => shape (a,b,c)
-			// muldia::shape<T>((a,b,c)) => shape (a,b,c)
-			// muldia::shape<T>(T a)   => shape (a)
-			// muldia::shape<T>(std::initialize_list<T>({a,b,c})) => shape {a,b,c}
+			// muldia::shape([a,b,c]) => shape (a,b,c)
+			// muldia::shape(std::vector<__shape_t>(a,b,c)) => shape (a,b,c)
+			// muldia::shape((a,b,c)) => shape (a,b,c)
+			// muldia::shape(__shape_t a)   => shape (a)
+			// muldia::shape(std::initialize_list<__shape_t>({a,b,c})) => shape {a,b,c}
 			//
 			//
 			shape() = delete;
@@ -39,15 +41,20 @@ namespace __md{
 					_shape.push_back(_t);
 				}
 			}
-			shape(std::vector<__shape_t> _vt) : _shape{_vt},_shape_size{static_cast<__shape_size_t>(_vt.size())}{}
+			shape(__shape _vt) : _shape{_vt},_shape_size{static_cast<__shape_size_t>(_vt.size())}{}
 			shape(__shape_t* _pt,__shape_size_t _ts=1) : _shape_size{_ts}{
 				for (__shape_size_t i=0;i!=_ts;++i){
 					_shape.push_back(*(_pt+i));
 				}
 			}
+			shape(__shape_list __il) : _shape_size{static_cast<__shape_size_t>(__il.size())}{
+				for (__shape_size_t i=0;i!=_shape_size;++i){
+					_shape.push_back(*(__il.begin() + i));
+				}
+			}
 			shape(const shape& __s) : _shape{__s._shape},_shape_size{__s._shape_size}{}
 			shape(shape&& __s) : _shape{__s._shape},_shape_size{__s._shape_size}{
-				__s._shape = std::vector<__shape_t>{};
+				__s._shape = __shape{};
 				__s._shape_size = 0;
 			}
 			shape& operator=(const shape& __s){
@@ -74,22 +81,22 @@ namespace __md{
 				}
 				return _shape[i];
 			}
-			std::vector<__shape_t> operator()() const{ return _shape; }
+			__shape operator()() const{ return _shape; }
 			__shape_size_t ndim() { return _shape_size; }
 			std::string str(){
 				std::string res{};
-				res += "{";
-				for (auto __sp : _shape){
+				res += "(";
+				for (const auto& __sp : _shape){
 					res += std::to_string(__sp);
-					if (__sp != _shape.back())
+					if (&__sp != &(_shape.back()))
 						res += ",";
 				}
-				res += "}";
+				res += ")";
 				return res;
 			}
 
 		private:
-			std::vector<__shape_t> _shape;
+			__shape _shape;
 			__shape_size_t   _shape_size;
 	
 	}; // class shape
--- test/test.cpp
+++ test/test.cpp
@@ -4,5 +4,14 @@
 int main()
 {
 	__md::shape a{5};
+	__md::shape b{3,10};
+	__md::shape c{std::vector<unsigned int>{1,3,512,512}};
+	unsigned int e[3] = {10,12,31};
+	__md::shape d{e,3};
+	__md::shape f{std::initializer_list<unsigned int>{1,3,512,512}};
 	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << c << std::endl;
+	std::cout << d << std::endl;
+	std::cout << f << std::endl;
 }
